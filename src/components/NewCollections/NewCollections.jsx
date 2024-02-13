3dc0a994a0e8f4479b4b66ca4aa1bc2d71dbeb45
Vikash Sharma <vs8824394430@gmail.com>
2024-02-13 20:59:09 +0530

    made responsive frontend

--- src/components/NewCollections/NewCollections.jsx
+++ src/components/NewCollections/NewCollections.jsx
@@ -4,7 +4,7 @@ import new_collection from '../Assests/new_collections'
 import Item from '../Item/Item'
 export const NewCollections = () => {
   return (
-    <div className="new-collection">
+    <div className="new-collections">
      <h1>NEW COLLECTIONS</h1>
      <hr />
      <div className="collections">
